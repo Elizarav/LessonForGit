bacb02e53ec8fded8678635d56f11c82e0d5dfef
Alexandr <elizarav@mail.ru>
2023-08-17 19:34:23 +0400

    Выполнение первого домашнего задания

---
+++ src/com/company/lessonOne/HomeWorkOne.java
@@ -0,0 +1,10 @@
+package com.company.lessonOne;
+
+public class HomeWorkOne {
+    public static void main(String[] args) {
+        taskOne();
+    }
+    public static void taskOne() {
+        System.out.println("Решение первой задачи");
+    }
+}
